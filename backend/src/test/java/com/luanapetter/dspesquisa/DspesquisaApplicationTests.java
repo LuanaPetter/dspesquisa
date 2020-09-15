7c11722f10153bb490e67783f5f02420be8cb6a3
Luana Petter <petter.luana@gmail.com>
2020-09-15 17:29:44 -0300

    Project created

---
+++ backend/src/test/java/com/luanapetter/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanapetter.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
