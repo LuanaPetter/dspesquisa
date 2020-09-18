a57530a84fbbe8bbe7e5d8a68f6cb4f438a89db8
Luana Petter <petter.luana@gmail.com>
2020-09-18 16:58:25 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -24,7 +24,7 @@ const initialPieData = {
 }
 
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds-luana.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
